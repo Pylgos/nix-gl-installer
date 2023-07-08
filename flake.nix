58284c095805d0dfaee6f6cb68df29b80f7f8582
Pylgos <43234674+Pylgos@users.noreply.github.com>
2023-07-08 15:09:47 +0900

    default nixpkgs version to 23.05

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
