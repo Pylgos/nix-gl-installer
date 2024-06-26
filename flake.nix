441d5eb500f5e75f10e12baf7928cfd50ead743f
Pylgos <43234674+Pylgos@users.noreply.github.com>
2024-06-26 23:32:27 +0900

    chore: update nixpkgs version to 24.05

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
